c9f46e20f53ec142535fe60bd73529547aee04ea
MartinAngelo <gelomartin163@gmail.com>
2021-07-07 09:26:14 +0800

    gawa ni ronjay

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import "./App.css";
+
+
+
+// import Sidebar from "./Sidebar"; //
+import Feed from "./Feed";
+export default function App() {
+    return ( 
+        < div className = "App" >
+     
+        <Feed /> 
+        </div>
+    );
+}
